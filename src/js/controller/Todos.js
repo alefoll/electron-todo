78ab5c2da453b3afa168a5daca4e93deae108a3d
Alexandre Le Foll <alefoll@doyoudreamup.com>
2016-11-28 15:19:44 +0100

    Version 0.2.0

--- src/js/controller/Todos.js
+++ src/js/controller/Todos.js
@@ -1,5 +1,30 @@
 (() => {
+    const { remote } = require('electron');
+
+    const moment = remote.require('moment');
+
     angular.module('Todos')
+        .config(['$mdDateLocaleProvider', function($mdDateLocaleProvider) {
+            $mdDateLocaleProvider.months      = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+            $mdDateLocaleProvider.shortMonths = ["janv.",   "févr.",   "mars", "avr.",  "mai", "juin", "juil.",   "août",  "sept.",    "oct.",    "nov.",     "déc."];
+
+            $mdDateLocaleProvider.days      = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"];
+            $mdDateLocaleProvider.shortDays = ["Di",       "Lu",    "Ma",    "Me",       "Je",    "Ve",        "Sa"];
+
+            $mdDateLocaleProvider.firstDayOfWeek = 1;
+
+            $mdDateLocaleProvider.weekNumberFormatter = (weekNumber) => {
+                return 'Semaine ' + weekNumber;
+            }
+
+            $mdDateLocaleProvider.formatDate = function(date) {
+                const m = moment(date);
+                return m.isValid() ? m.format('L') : '';
+            };
+
+            $mdDateLocaleProvider.msgCalendar     = "Calendrier";
+            $mdDateLocaleProvider.msgOpenCalendar = "Ouvrir le calendrier";
+        }])
         .controller('TodosController', ['$scope', '$mdDialog', ($scope, $mdDialog) => {
             $scope.add     = add;
             $scope.cancel  = cancel;
@@ -12,6 +37,10 @@
             $scope.todos    = JSON.parse(localStorage.getItem('todos')) || [];
 
             $scope.$watchCollection('todos', () => {
+                $scope.todos.sort((a, b) => {
+                    return (a.date.getTime() === b.date.getTime()) ? a.task.toLowerCase() > b.task.toLowerCase() : a.date.getTime() > b.date.getTime();
+                });
+
                 localStorage.setItem('todos', JSON.stringify($scope.todos));
             })
 
