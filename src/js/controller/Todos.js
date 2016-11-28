4e5e79cfe9ff0483d4c5bcc83987f7a0041207f2
Alexandre Le Foll <alexandre@le-foll.fr>
2016-11-28 23:30:57 +0100

    Version 0.5.0

--- src/js/controller/Todos.js
+++ src/js/controller/Todos.js
@@ -1,6 +1,9 @@
 (() => {
-    const { remote } = require('electron');
+    const { remote, shell } = require('electron');
 
+    const { Menu, MenuItem } = remote;
+
+    const Config = remote.require('./electron/Config');
     const moment = remote.require('moment');
 
     angular.module('Todos')
@@ -26,26 +29,54 @@
             $mdDateLocaleProvider.msgOpenCalendar = "Ouvrir le calendrier";
         }])
         .controller('TodosController', ['$scope', '$mdDialog', ($scope, $mdDialog) => {
-            $scope.add     = add;
-            $scope.cancel  = cancel;
-            $scope.done    = done;
-            $scope.showAdd = showAdd;
+            $scope.add             = add;
+            $scope.close           = close;
+            $scope.done            = done;
+            $scope.edit            = edit;
+            $scope.isYoutrackIssue = isYoutrackIssue;
+            $scope.openExternal    = shell.openExternal;
+            $scope.showAdd         = showAdd;
+            $scope.showEdit        = showEdit;
 
-            $scope.date     = '';
-            $scope.minDate  = new Date();
-            $scope.task     = '';
-            $scope.todos    = JSON.parse(localStorage.getItem('todos')) || [];
+            $scope.date        = '';
+            $scope.minDate     = new Date();
+            $scope.task        = '';
+            $scope.todos       = JSON.parse(localStorage.getItem('todos')) || [];
+            $scope.youtrackURL = Config.get('youtrack.url');
 
-            $scope.$watchCollection('todos', () => {
+            $scope.$watchCollection('todos', _updateAndSave)
+
+            for(let todo of $scope.todos)
+                todo.date = new Date(todo.date);
+
+            const menu = new Menu();
+
+            let index;
+
+            menu.append(new MenuItem({
+                label: 'Edit',
+                click() {
+                    showEdit(index)
+                }
+            }));
+
+            window.addEventListener('contextmenu', (event) => {
+                if (event.target.classList.contains('task')) {
+                    event.preventDefault();
+
+                    index = event.target.dataset.index;
+
+                    menu.popup(remote.getCurrentWindow());
+                }
+            }, false)
+
+            function _updateAndSave() {
                 $scope.todos.sort((a, b) => {
                     return (a.date.getTime() === b.date.getTime()) ? a.task.toLowerCase() > b.task.toLowerCase() : a.date.getTime() > b.date.getTime();
                 });
 
                 localStorage.setItem('todos', JSON.stringify($scope.todos));
-            })
-
-            for(let todo of $scope.todos)
-                todo.date = new Date(todo.date);
+            }
 
             function add() {
                 $scope.todos.push({
@@ -53,16 +84,18 @@
                     created : new Date(),
                     task    : $scope.task,
                     date    : $scope.date
-                })
+                });
 
-                cancel();
+                close();
             }
 
-            function cancel() {
+            function close() {
                 $mdDialog.hide();
 
                 $scope.task = '';
                 $scope.date = '';
+
+                _updateAndSave();
             }
 
             function done(todo) {
@@ -71,9 +104,34 @@
                 $scope.todos.splice(index, 1);
             }
 
+            function edit() {
+                $scope.todos[index].task = $scope.task;
+                $scope.todos[index].date = $scope.date;
+
+                close();
+            }
+
+            function isYoutrackIssue(task) {
+                return (Config.get('youtrack.enabled')) ? (/([A-Z]+)-([0-9]+)/.test(task)) : false;
+            }
+
             function showAdd() {
                 $mdDialog.show({
-                    templateUrl         : 'newTodo.html',
+                    templateUrl         : 'modalAdd.html',
+                    preserveScope       : true,
+                    scope               : $scope,
+                    parent              : angular.element(document.body),
+                    clickOutsideToClose : false,
+                    fullscreen          : false
+                })
+            }
+
+            function showEdit(index) {
+                $scope.task = $scope.todos[index].task;
+                $scope.date = $scope.todos[index].date;
+
+                $mdDialog.show({
+                    templateUrl         : 'modalEdit.html',
                     preserveScope       : true,
                     scope               : $scope,
                     parent              : angular.element(document.body),
